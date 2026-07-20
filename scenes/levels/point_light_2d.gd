a1358494e3df85f4bc4655acdc23d47e70015efb
michael <michaelprincesort@gmail.com>
2026-07-20 10:33:16 +0200

    added tutorial Ui to tutorial map

--- scenes/levels/point_light_2d.gd
+++ scenes/levels/point_light_2d.gd
@@ -18,7 +18,7 @@ func unhandled_input() -> void:
 			.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 			
 		# 2. Wait at the expanded size for 5 seconds inside the tween itself
-		tween.tween_interval(5.0)
+		tween.tween_interval(3.0)
 		
 		# 3. Smoothly shrink back to original size over 0.5 seconds
 		tween.tween_property(self, "texture_scale", original_scale, 1.5)\
--- scripts/levels/toturial_map.gd
+++ scripts/levels/toturial_map.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
+@onready var move_t = $"TutorialUI/move turoral"
+@onready var echo_t = $"TutorialUI/Echo tutorial"
+@onready var fly_t = $"TutorialUI/second tutorial"
+@onready var help_page = $"TutorialUI/Help page"
+@onready var aim = $"TutorialUI/Goal page"
+
 var scene_name = "tutorial_map"
 
 var next_level = false
 var scene_transition
+var tutorial = true
+var transition_timer = 5
+var count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$TutorialUI.visible = true
 	scene_transition = $Level_transition
 	scene_transition.body_entered.connect(_on_level_transition_body_entered)
+
+func _process(delta: float) -> void:
+	count += delta * 2
+	
+	if tutorial and count > transition_timer:
+		tutorial_transition()
 
 func _on_level_transition_body_entered(body: CharacterBody2D) -> void:
 	if body.ID == "player":
@@ -15,3 +31,33 @@ func _on_level_transition_body_entered(body: CharacterBody2D) -> void:
 
 func move_to_next_level() -> bool:
 	return next_level
+
+func tutorial_transition():
+	var up = Input.is_key_label_pressed(KEY_SPACE) or Input.is_action_just_pressed("ui_up")
+	var echo = Input.is_key_label_pressed(KEY_E)
+	var left = Input.is_key_label_pressed(KEY_A) or Input.is_action_just_pressed("ui_left")
+	var right = Input.is_key_label_pressed(KEY_D) or Input.is_action_just_pressed("ui_right")
+	
+	if help_page.visible:
+		if count > 12:
+			help_page.visible = false
+			count = 0
+		return
+	
+	if left or right:
+		if move_t.visible:
+			fly_t.visible = true
+			count = 0
+		move_t.visible = false
+		aim.visible = false
+		
+	if up:
+		if fly_t.visible:
+			echo_t.visible = true
+			count = 0
+		fly_t.visible = false
+		
+	if echo:
+		echo_t.visible = false
+		tutorial = false
+		count = 0
